ca9fbffca5fe755973fa4bc98e6e1ab9832222cf
ilyukevich <9009901@gmail.com>
2022-07-28 18:45:52 +0300

    add retrieve, put, delete endpoints

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -1,8 +1,7 @@
 from typing import List, Optional
-from fastapi import APIRouter
-from fastapi import Depends
+from fastapi import APIRouter, Depends, Response, status
 
-from ..models.operations import Operation, OperationKind, OperationCreate
+from ..models.operations import Operation, OperationKind, OperationCreate, OperationUpdate
 from ..services.operations import OperationsService
 
 
@@ -19,9 +18,38 @@ def get_operations(
     return service.get_list(kind=kind)
 
 
+@router.get('/{operation_id}', response_model=Operation)
+def get_operations(
+        operation_id: int,
+        service: OperationsService = Depends()
+):
+    return service.get(operation_id)
+
+
 @router.post('/', response_model=Operation)
 def create_operation(
         operation_data: OperationCreate,
         service: OperationsService = Depends(),
 ):
     return service.create(operation_data)
+
+
+@router.put('/{operation_id}', response_model=Operation)
+def update_operation(
+        operation_id: int,
+        operation_data: OperationUpdate,
+        service: OperationsService = Depends(),
+):
+    return service.update(
+        operation_id,
+        operation_data
+    )
+
+
+@router.delete('/{operation_id}')
+def delete_operation(
+        operation_id: int,
+        service: OperationsService = Depends(),
+):
+    service.delete(operation_id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- src/workshop/services/operations.py
+++ src/workshop/services/operations.py
@@ -1,15 +1,26 @@
 from typing import List, Optional
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from .. import tables
 from ..database import get_session
-from ..models.operations import OperationKind, OperationCreate
+from ..models.operations import OperationKind, OperationCreate, OperationUpdate
 
 
 class OperationsService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
+
+    def _get(self, operation_id: int) -> tables.Operation:
+        operation = (
+            self.session
+            .query(tables.Operation)
+            .filter_by(id=operation_id)
+            .first()
+        )
+        if not operation:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return operation
 
     def get_list(self, kind: Optional[OperationKind] = None) -> List[tables.Operation]:
         query = self.session.query(tables.Operation)
@@ -22,8 +33,32 @@ class OperationsService:
         operations = query.all()
         return operations
 
+    def get(self, operation_id: int) -> tables.Operation:
+        # operation = (
+        #     self.session
+        #     .query(tables.Operation)
+        #     .filter_by(id=operation_id)
+        #     .first()
+        # )
+        # if not operation:
+        #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        # return operation
+        return self._get(operation_id)
+
     def create(self, operation_data: OperationCreate) -> tables.Operation:
         operation = tables.Operation(**operation_data.dict())
         self.session.add(operation)
         self.session.commit()
         return operation
+
+    def update(self, operation_id: int, operation_data: OperationUpdate) -> tables.Operation:
+        operation = self._get(operation_id)
+        for field, value in operation_data:
+            setattr(operation, field, value)
+        self.session.commit()
+        return operation
+
+    def delete(self, operation_id: int):
+        operation = self._get(operation_id)
+        self.session.delete(operation)
+        self.session.commit()
